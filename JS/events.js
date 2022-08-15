311e61b36a372062a0489765d6e4caefd724d11b
ahkamulbd <ahkamulbd@gmail.com>
2022-08-15 14:47:43 +0600

    onclick handler addressed according to Lecture 25-2

--- JS/events.js
+++ JS/events.js
@@ -1,1 +1,24 @@
 console.log('This is separate JS file');
+
+// option 2:
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3:
+const makeBlueButton = document.getElementById('make-blue');
+//console.log(makeBlueButton);
+
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option 3 Another
+const purpleButton = document.getElementById('make-purple');
+// console.log(purpleButton);
+purpleButton.onclick = function
+    makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
